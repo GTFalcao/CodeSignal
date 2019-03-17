186a990255ec11fc7c944c9f857bc8880ee0169b
GTFalcao <gtfalcao96@gmail.com>
2019-03-17 11:56:14 -0300

    Arcade - Intro - Stage 4

--- arcade/intro/stage4/palindromeRearranging.js
+++ arcade/intro/stage4/palindromeRearranging.js
@@ -8,37 +8,37 @@ A palindrome is a string that reads the same left-to-right and right-to-left.
 
 var charCount = [];
 
-function checkChar (string, char) {
+function checkChar(string, char) {
     for (let i = 0; i < charCount.length; i++) {
-        if(charCount[i][0] == char)
+        if (charCount[i][0] == char)
             return i;
     }
-           
+
     return -1;
 }
 
 var oddChars = 0;
 
 function palindromeRearranging(inputString) {
-    
+
     for (let i = 0; i < inputString.length; i++) {
         let pos = checkChar(inputString, inputString[i]);
-        
-        if( (!i) || (pos == -1) )
+
+        if ((!i) || (pos == -1))
             charCount.push([inputString[i], 1]);
-        
+
         else
             charCount[pos][1]++;
     }
-    
+
     for (let i = 0; i < charCount.length; i++) {
-        if(charCount[i][1] % 2) {
-            if(!oddChars)
+        if (charCount[i][1] % 2) {
+            if (!oddChars)
                 oddChars = 1;
             else
                 return false;
         }
     }
-    
+
     return true;
 }
